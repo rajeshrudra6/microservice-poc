37e8bd072ce648f65586743e7138d0eb80152c8e
Rajeshrudra <Rajeshrudra@Rajeshrudra-PC>
2018-03-29 00:06:21 +0530

    Second commit

--- src/main/java/org/arpit/java2blog/SpringBootAngularJSApplication.java
+++ src/main/java/org/arpit/java2blog/SpringBootAngularJSApplication.java
@@ -2,6 +2,7 @@ package org.arpit.java2blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is my second commit and it will be shown only after i commit
 @SpringBootApplication
 public class SpringBootAngularJSApplication {
 
