479b6e1ba08dad35445ff55931eb622c851d4b56
Rajeshrudra <Rajeshrudra@Rajeshrudra-PC>
2018-03-29 00:14:28 +0530

    third commit

--- src/main/java/org/arpit/java2blog/SpringBootAngularJSApplication.java
+++ src/main/java/org/arpit/java2blog/SpringBootAngularJSApplication.java
@@ -2,7 +2,8 @@ package org.arpit.java2blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// This is my second commit and it will be shown only after i commit
+// This is my second commit and it will be shown only after i commit.
+// this is third commit and color change
 @SpringBootApplication
 public class SpringBootAngularJSApplication {
 
